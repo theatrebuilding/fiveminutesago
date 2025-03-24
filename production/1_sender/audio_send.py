8585fd3f878343bbcedb9f4725cb35d37702a7df
Jazbo Gross <jazbogross@gmail.com>
2025-03-24 15:50:09 +0100

    updates

--- production/1_sender/audio_send.py
+++ production/1_sender/audio_send.py
@@ -60,8 +60,8 @@ def main():
     # Build the GStreamer command for sending audio
     send_audio_cmd = (
         f"gst-launch-1.0 -v {source} device={device} ! "
-        f"audioconvert ! audioresample ! "
         f"audio/x-raw,format={audio_format},channels={audio_channels},rate={audio_rate} ! "
+        f"audioconvert ! audioresample ! "
         f"rtpL16pay ! "
         f"srtsink uri='srt://{server_ip}:{audio_send_port}?mode=caller&{streaming_settings}'"
     )
