b884cf07803d26042bcbb0e68b0da19eabf55ec4
Jazbo Gross <jazbogross@gmail.com>
2025-03-25 15:37:00 +0100

    updates

--- production/1_sender/audio_send.py
+++ production/1_sender/audio_send.py
@@ -68,19 +68,23 @@ def main():
     #     f"srtsink uri='srt://{server_ip}:{audio_send_port}?mode=caller&{streaming_settings}'"
     # )
 
+
     send_audio_cmd = (
         f"gst-launch-1.0 -v "
         f"{source} device={device} ! "
         f"audioconvert ! audioresample ! "
         f"audio/x-raw,format={audio_format},channels=1,rate={audio_rate} ! "
-        f"webrtcdsp name=aec aec=1 agc=1 noise-suppression=1 ! "
+        f"webrtcdsp name=aec agc=1 noise-suppression=1 ! "
         f"audioconvert ! audio/x-raw,channels=2 ! "
         f"rtpL16pay ! "
-        f"srtsink uri='srt://{server_ip}:{audio_send_port}?mode=caller&{streaming_settings}' !"
+        f"webrtcechoprobe ! "
+        f"srtsink uri='srt://{server_ip}:{audio_send_port}?mode=caller&{streaming_settings}' ! "
+        
         f"srtsrc uri='srt://{server_ip}:{audio_loopback_port}?mode=caller' ! "
         f"audioconvert ! audioresample ! "
         f"aec.ref."
 )
+    # AUDIO ERROR: WARNING: erroneous pipeline: no property "aec" in element "webrtcdsp"
 
     print("Starting audio sending pipeline...")
     print(f"Host: {server_ip}, Port: {audio_send_port}")
