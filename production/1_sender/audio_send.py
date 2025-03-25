96dcd10c4913dae9957b727d474d5c00ddfb6187
Jazbo Gross <jazbogross@gmail.com>
2025-03-25 16:31:06 +0100

    updates

--- production/1_sender/audio_send.py
+++ production/1_sender/audio_send.py
@@ -60,25 +60,13 @@ def main():
     device = args.device
 
     # Build the GStreamer command for sending audio
-    # send_audio_cmd = (
-    #     f"gst-launch-1.0 -v {source} device={device} ! "
-    #     f"audioconvert ! audioresample ! "
-    #     f"audio/x-raw,format={audio_format},channels=1,rate={audio_rate} ! audioconvert ! audio/x-raw,channels=2 ! "
-    #     f"rtpL16pay ! "
-    #     f"srtsink uri='srt://{server_ip}:{audio_send_port}?mode=caller&{streaming_settings}'"
-    # )
-
-
-    send_audio_cmd = (
-       f"gst-launch-1.0 -v "
-       f"{source} device={device} ! "
-       f"audioconvert ! audioresample ! "
-       f"audio/x-raw,format={audio_format},channels=1,rate={audio_rate} ! "
-       f"webrtcdsp probe=webrtcechoprobe0 ! "
-       f"audioconvert ! audio/x-raw,channels=2 ! "
-       f"rtpL16pay ! "
-       f"srtsink uri='srt://{server_ip}:{audio_send_port}?mode=caller&{streaming_settings}'"
-    )
+     send_audio_cmd = (
+         f"gst-launch-1.0 -v {source} device={device} ! "
+         f"audioconvert ! audioresample ! "
+         f"audio/x-raw,format={audio_format},channels=1,rate={audio_rate} ! audioconvert ! audio/x-raw,channels=2 ! "
+         f"rtpL16pay ! "
+         f"srtsink uri='srt://{server_ip}:{audio_send_port}?mode=caller&{streaming_settings}'"
+     )
 
 
     print("Starting audio sending pipeline...")
--- production/3_receiver/audio_receive.py
+++ production/3_receiver/audio_receive.py
@@ -28,14 +28,13 @@ def build_audio_pipeline(country, device):
         receive_port = config.get("ports", {}).get("audio_receive_dk")
     
     pipeline = f"""
-        gst-launch-1.0 -v srtsrc uri="srt://{server_address}:{receive_port}?mode=caller&latency=1000&maxbw=0"
+        srtsrc uri="srt://{server_address}:{receive_port}?mode=caller&latency=1000&maxbw=0"
             ! queue max-size-time=200000000
             ! application/x-rtp,media=audio,clock-rate=32000,encoding-name=L16,channels=2
             ! rtpL16depay
             ! audioconvert
             ! audioresample
-            ! webrtcechoprobe name=webrtcechoprobe0
-            ! audioconvert
+            ! webrtcechoprobe 
             ! alsasink device="{device}"
     """
     return pipeline.strip()
