abe7d8a777bc670e158e39ef34968cc99a774f80
Jazbo Gross <jazbogross@gmail.com>
2025-03-26 14:01:53 +0100

    updates

--- production/2_server/video_pipeline.py
+++ production/2_server/video_pipeline.py
@@ -25,14 +25,14 @@ def build_video_pipeline():
     # These pipelines relay the stream between ports with low latency and bounded buffering.
     # Rembember to add keep-listening=true to the srtsink elements to prevent the pipeline from stopping. It needs to be added to the uri settings.
     pipeline1 = (
-        f'srtsrc uri="srt://:{video_send_tn}?mode=listener&{streaming_settings}" wait-for-connection=false  '
+        f'srtsrc uri="srt://:{video_send_tn}?mode=listener&latency=100" wait-for-connection=false  '
         f'! queue max-size-time=2000000000 max-size-buffers=200 '
-        f'! srtsink uri="srt://:{video_receive_dk}?mode=listener&{streaming_settings}" wait-for-connection=false '
+        f'! srtsink uri="srt://:{video_receive_dk}?mode=listener&latency=100" wait-for-connection=false '
     )
     pipeline2 = (
-        f'srtsrc uri="srt://:{video_send_dk}?mode=listener&{streaming_settings}" wait-for-connection=false  '
+        f'srtsrc uri="srt://:{video_send_dk}?mode=listener&latency=100" wait-for-connection=false  '
         f'! queue max-size-time=2000000000 max-size-buffers=200 '
-        f'! srtsink uri="srt://:{video_receive_tn}?mode=listener&{streaming_settings}" wait-for-connection=false '
+        f'! srtsink uri="srt://:{video_receive_tn}?mode=listener&latency=100" wait-for-connection=false '
     )
     return pipeline1, pipeline2
 
