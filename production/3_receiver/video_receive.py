b8593bd97f6e66db2a39d2e4be4cf26a3a402d72
Jazbo Gross <jazbogross@gmail.com>
2025-03-28 13:19:55 +0100

    cairo overlay

--- production/3_receiver/video_receive.py
+++ production/3_receiver/video_receive.py
@@ -34,7 +34,7 @@ class VideoReceiver:
             self.receive_port = config.get("ports", {}).get("video_receive_dk")
         
         pipeline_str = f"""
-            videotestsrc pattern=ball 
+            srtsrc uri="srt://{self.server_address}:{self.receive_port}?mode=caller&latency=100" 
                 ! queue max-size-time=2000000000 max-size-buffers=500 
                 ! tsdemux name=demux 
                 demux. ! queue ! h264parse config-interval=1 
