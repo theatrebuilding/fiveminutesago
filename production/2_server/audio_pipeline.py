98bc46e933a9f015fd0261f9c38649d5233e92cc
Jazbo Gross <jazbogross@gmail.com>
2025-04-03 12:28:33 +0200

    remove wait for connection from server srt sinks

--- production/2_server/audio_pipeline.py
+++ production/2_server/audio_pipeline.py
@@ -73,14 +73,14 @@ def build_audio_pipeline():
           audioconvert ! audioresample !
           audio/x-raw,format={audio_format},channels={channels},rate={clock_rate} !
           rtpL16pay !
-          srtsink name=a_recv_dk uri=srt://:{audio_receive_dk}?mode=listener wait-for-connection=false
+          srtsink name=a_recv_dk uri=srt://:{audio_receive_dk}?mode=listener
 
         tee_dk. ! queue !
           webrtcdsp probe=probe_dk compression-gain-db={compression_level} delay-agnostic={delay_agnostic} echo-cancel={echo_cancel} echo-suppression-level={echo_suppression} experimental-agc={experimental_agc} extended-filter={extended_filter} gain-control={gain_control} gain-control-mode={gain_control_mode} high-pass-filter={high_pass_filter} limiter={limiter} noise-suppression={noise_suppression} noise-suppression-level={noise_suppression_level} startup-min-volume={startup_min_volume} target-level-dbfs={target_level_dbfs} !
           audioconvert ! audioresample !
           audio/x-raw,format={audio_format},channels={channels},rate={clock_rate} !
           rtpL16pay !
-          srtsink name=a_recv_tn uri=srt://:{audio_receive_tn}?mode=listener wait-for-connection=false
+          srtsink name=a_recv_tn uri=srt://:{audio_receive_tn}?mode=listener
 
     """
     return pipeline.strip()
