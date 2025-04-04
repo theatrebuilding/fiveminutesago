74869c9a5078673980860dcf20c023b40cb34a92
Jazbo Gross <jazbogross@gmail.com>
2025-04-04 12:09:33 +0200

    video receive fallback

--- production/3_receiver/video_receive.py
+++ production/3_receiver/video_receive.py
@@ -37,7 +37,7 @@ class VideoReceiver:
         # Fallback branch: videotestsrc.
         # We name the last queue in the primary branch "primary_queue" so we can attach a pad probe.
         pipeline_str = f"""
-            input-selector name=selector ! kmssink connector-id=32 force-modesetting=false sync=false
+            input-selector name=selector ! kmssink connector-id=41 force-modesetting=false sync=false
             srtsrc uri="srt://{self.server_address}:{self.receive_port}?mode=caller&latency=100"
                 ! queue max-size-time=2000000000 max-size-buffers=500 
                 ! tsdemux name=demux 
