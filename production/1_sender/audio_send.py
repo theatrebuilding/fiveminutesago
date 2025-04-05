e561de88c2e26c1825df6c832c275caa6043baaf
Jazbo Gross <jazbogross@gmail.com>
2025-04-05 15:15:31 +0200

    buffer

--- production/1_sender/audio_send.py
+++ production/1_sender/audio_send.py
@@ -75,7 +75,7 @@ class AudioSender:
 
         pipeline_str = f"""
             srtsrc uri="srt://{self.server_ip}:{self.audio_recv_port}?mode=caller" wait-for-connection=false
-                ! queue
+                ! queue max-size-time=2000000000 max-size-buffers=500
                 ! application/x-rtp,media=audio,clock-rate={audio_rate},encoding-name={encoding_name},channels={channels}
                 ! rtpL16depay
                 ! audioconvert
