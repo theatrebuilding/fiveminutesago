3ea19d2e37129964a20ca4f7283a42b7734099b4
Jazbo Gross <jazbogross@gmail.com>
2025-04-08 15:07:55 +0200

    update

--- production/1_sender/send_audio.py
+++ production/1_sender/send_audio.py
@@ -13,6 +13,14 @@ from config_loader import load_config
 # Initialize GStreamer.
 Gst.init(None)
 
+# Ensure the parent directory is in sys.path to import config_loader.
+script_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.abspath(os.path.join(script_dir, ".."))
+if parent_dir not in sys.path:
+    sys.path.insert(0, parent_dir)
+
+from config_loader import load_config
+
 #########################################
 # AudioSender Class
 #########################################
