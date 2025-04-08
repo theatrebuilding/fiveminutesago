46afc6a77ae51550b875ad485eef63b30ddefccb
Jazbo Gross <jazbogross@gmail.com>
2025-04-08 18:47:16 +0200

    audio stream

--- production/1_sender/send_audio.py
+++ production/1_sender/send_audio.py
@@ -79,14 +79,29 @@ class AudioSender:
         echo_cancel = dsp_cfg.get("echo-cancel", True)
 
         pipeline_str = f"""
-        srtsrc uri="srt://:8806?mode=listener" wait-for-connection=false !
-          queue !
-          application/x-rtp,media=audio,clock-rate={audio_rate},encoding-name={encoding_name},channels={channels} !
-          rtpL16depay !
-          audioconvert !
-          audioresample !
-          lamemp3enc bitrate=128 !
-          shout2send ip=s40.myradiostream.com port=23058 mount=/stream password=5e4ThU3VW
+            srtsrc uri="srt://{self.server_ip}:{self.audio_recv_port}?mode=caller" wait-for-connection=false 
+                ! queue max-size-time=2000000000 max-size-buffers=500
+                ! application/x-rtp,media=audio,clock-rate={audio_rate},encoding-name={encoding_name},channels={channels}
+                ! rtpL16depay
+                ! audioconvert
+                ! audioresample
+                ! audio/x-raw,format=S16LE,channels={channels},rate={audio_rate}
+                ! webrtcechoprobe
+                ! queue
+                ! alsasink device={self.device} async=true
+
+            alsasrc card-name="Scarlett 8i6 USB"
+                ! queue
+                ! audioconvert
+                ! audioresample
+                ! audio/x-raw,format=S16LE,channels={channels},rate={audio_rate}
+                ! webrtcdsp echo-cancel={str(echo_cancel).lower()}
+                ! queue
+                ! audioconvert
+                ! audioresample
+                ! audio/x-raw,format={audio_format},channels={channels},rate={audio_rate}
+                ! rtpL16pay
+                ! srtsink uri="srt://{self.server_ip}:{self.audio_send_port}?mode=caller&{streaming_settings}"
         """
         return pipeline_str.strip()
 
