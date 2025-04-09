4ba8e516810d20c3a5af946dd9e21d2ee12951a6
Jazbo Gross <jazbogross@gmail.com>
2025-04-09 15:11:50 +0200

    revert port

--- production/2_server/video_pipeline.py
+++ production/2_server/video_pipeline.py
@@ -27,16 +27,16 @@ recorded_file_dk = "/mnt/tbdrive/video/video_dk.ts"
 def build_video_pipeline():
     # These pipelines relay the stream between ports with low latency and bounded buffering.
     pipeline1 = (
-        f'srtsrc name=v_send_tn uri="srt://0.0.0.0:{video_send_tn}?mode=listener&latency=100" '
+        f'srtsrc name=v_send_tn uri="srt://:{video_send_tn}?mode=listener&latency=100" '
         f'! queue max-size-time=2000000000 max-size-buffers=200 '
         f'! tee name=tee_tn '
-        f' tee_tn. ! queue ! srtsink name=v_recv_dk uri="srt://0.0.0.0:{video_receive_dk}?mode=listener&latency=100"'   
+        f' tee_tn. ! queue ! srtsink name=v_recv_dk uri="srt://:{video_receive_dk}?mode=listener&latency=100"'   
     )
     pipeline2 = (
-        f'srtsrc name=v_send_dk uri="srt://0.0.0.0:{video_send_dk}?mode=listener&latency=100" '
+        f'srtsrc name=v_send_dk uri="srt://:{video_send_dk}?mode=listener&latency=100" '
         f'! queue max-size-time=2000000000 max-size-buffers=200 '
         f'! tee name=tee_dk '
-        f' tee_dk. ! queue ! srtsink name=v_recv_tn uri="srt://0.0.0.0:{video_receive_tn}?mode=listener&latency=100"'  
+        f' tee_dk. ! queue ! srtsink name=v_recv_tn uri="srt://:{video_receive_tn}?mode=listener&latency=100"'  
     )
     return pipeline1, pipeline2
 
