f1139418a5cc68f1ba83a0c02d791ef03b8ca629
Jazbo Gross <jazbogross@gmail.com>
2025-04-09 17:27:14 +0200

    remove server latency video

--- production/2_server/video_pipeline.py
+++ production/2_server/video_pipeline.py
@@ -30,13 +30,13 @@ def build_video_pipeline():
         f'srtsrc name=v_send_tn uri="srt://:{video_send_tn}?mode=listener" '
         f'! queue max-size-time=2000000000 max-size-buffers=200 '
         f'! tee name=tee_tn '
-        f' tee_tn. ! queue ! srtsink name=v_recv_dk uri="srt://:{video_receive_dk}?mode=listener&latency=100"'   
+        f' tee_tn. ! queue ! srtsink name=v_recv_dk uri="srt://:{video_receive_dk}?mode=listener"'   
     )
     pipeline2 = (
         f'srtsrc name=v_send_dk uri="srt://:{video_send_dk}?mode=listener" '
         f'! queue max-size-time=2000000000 max-size-buffers=200 '
         f'! tee name=tee_dk '
-        f' tee_dk. ! queue ! srtsink name=v_recv_tn uri="srt://:{video_receive_tn}?mode=listener&latency=100"'  
+        f' tee_dk. ! queue ! srtsink name=v_recv_tn uri="srt://:{video_receive_tn}?mode=listener"'  
     )
     return pipeline1, pipeline2
 
