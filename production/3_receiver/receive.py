c93082c378ca51c6e44f7e611f107df5856fc119
Jazbo Gross <jazbogross@gmail.com>
2025-04-12 18:41:34 +0200

    set sync=true on receiver

--- production/3_receiver/receive.py
+++ production/3_receiver/receive.py
@@ -45,7 +45,7 @@ class VideoReceiver:
             self.receive_port = config.get("ports", {}).get("video_receive_dk")
         
         pipeline_str = f"""
-            input-selector name=selector ! kmssink 
+            input-selector name=selector ! kmssink sync=true
             srtsrc uri="srt://{self.server_address}:{self.receive_port}?mode=caller"
                 ! queue max-size-time=2000000000 max-size-buffers=500
                 ! tsdemux name=demux
